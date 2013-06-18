ba6b1de16b4ffedd7c99f34391a8d98fcc14845e
Muhilan MG <muhilanis@yahoo.co.in>
2013-06-18 16:36:31 -0500

    fixed map size

--- js/rohlxcustom.js
+++ js/rohlxcustom.js
@@ -1,22 +1,27 @@
 function initialize() {
   var mapOptions = {
-    zoom: 16,
+    zoom: 18,
     center: new google.maps.LatLng(8.148,77.568455),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
-  var map = new google.maps.Map($('#map-canvas'),
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+  /** Needed to bring the map into the view **/
+  $('#map-canvas')[0].scrollIntoView()
 }
 
 function loadScript() {
- 
+    
+  
+  if(!isGoogleAPILoadedAlready())
+  {
   var script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
       'callback=initialize';
   document.body.appendChild(script);
-
+  }
   
 } 
 
@@ -28,6 +33,17 @@ $("html, body").animate({ scrollTop: top }, 700);
 return false;
 });
 }
- 
+
+
+function isGoogleAPILoadedAlready(){
+    
+    if($('script[src*="maps.googleapis.com"]').val() === undefined)
+    {
+        return false;
+    }
+    return true;
+}
+
+
 $("#openmaps").unbind().click(loadScript);
 $(window).load(animate);
